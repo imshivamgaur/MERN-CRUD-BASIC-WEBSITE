438ae371f967cf610a6e66c8870daf4e64b08aaa
iamshivamgaur <shivam24august@gmail.com>
2024-11-10 02:10:59 +0530

    Card Modify

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -10,7 +10,7 @@ const Card = ({ product, colorMode, onDelete }) => {
       <div className={` ${colorMode === "dark" ? "text-white bg-gray-700": "text-black bg-gray-300" } hover:translate-y-[-5px] hover:scale-[1.02] hover:shadow-md hover:shadow-[#00000054]  transition-all duration-[.3s]  ease-linear  w-[300px] h-[auto]   p-5 rounded-lg`}>
         {/* <img src="https://images.unsplash.com/photo-1461141346587-763ab02bced9?q=80&w=1400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" /> */}
         <img className="w-full h-[180px] rounded-lg object-cover " src={product.image} alt="" />
-        <p className="font-semibold text-gray-100" >{product.name}</p>
+        <p className="font-semibold text-gray-100 capitalize" >{product.name}</p>
         <p>₹{product.price}</p>
         <div className="flex gap-2">
         <button className="px-2 bg-blue-500 text-white py-1 rounded-sm cursor-pointer mt-5">
